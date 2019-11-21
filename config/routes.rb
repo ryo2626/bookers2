3e9dd804ddfd772727a955408a61e65b2aec335e
吉田　凌 <yoshidaryo@yoshidaryounoMacBook-Air.local>
2019-11-22 00:35:19 +0900

    finish bookers2

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
+
+end
